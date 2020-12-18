f7a6ab64d693ce33e2fca58a9eba4ec721c5dbcf
Jones S. Croppper <callowcreation@gmail.com>
2020-12-18 00:38:31 -0800

    collision offset detection seperation of a set amount will result in a pop of the balloon

--- Assets/Scripts/Contacts.cs
+++ Assets/Scripts/Contacts.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+﻿using System.Linq;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -25,14 +25,39 @@ public class Contacts : MonoBehaviour
     [SerializeField]
     int m_Hits = 0;
 
+    [SerializeField]
+    float m_ResponseTime = 0.5f;
+    [SerializeField]
+    float m_ReflectThreshold = -0.15f;
+
     public int maxHits { get => m_MaxHits; }
     public int hits { get => m_Hits; }
 
+    Rigidbody2D rb2d = null;
+
+    void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
     void OnCollisionEnter2D(Collision2D collision)
     {
+        if (rb2d.isKinematic == true) return;
+
         if(collision.gameObject.layer == PLAYER_LAYER)
         {
             m_Hits += m_Power.player;
+
+            List<ContactPoint2D> items = collision.contacts.Where(x => x.separation < m_ReflectThreshold).ToList();
+
+            if(items.Count() > 0)
+            {
+                Debug.Log($"{items.Count} {items[0].separation} separation");
+            }
+
+            //rb2d.velocity = Vector3.Reflect(lastVelocity, collision.contacts[0].normal) * m_ReflectMultiplier;
+            rb2d.isKinematic = true;
+            Invoke("ResetCollision", m_ResponseTime);
         } 
         else if(collision.gameObject.layer == GROUND_LAYER)
         {
@@ -40,5 +65,16 @@ public class Contacts : MonoBehaviour
         }
 
         onContact?.Invoke();
+    }
+
+    void ResetCollision()
+    {
+        rb2d.isKinematic = false;
+    }
+
+    Vector2 lastVelocity;
+    void FixedUpdate()
+    {
+        lastVelocity = rb2d.velocity;
     }
 }
